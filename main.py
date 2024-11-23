112ecfa826a530b30f48fd16ff09606f64bfd0bf
Abraham Legorreta <abraham.legorreta.valenzuela@gmail.com>
2024-11-23 11:29:21 -0600

    feat: initial comment to help the evaluator to estimate the amount of time spent on the project

---
+++ main.py
@@ -0,0 +1,12 @@
+from datetime import timedelta, datetime
+from typing import Tuple, List
+
+
+def find_available_slots(
+        schedule: List[Tuple[datetime, datetime]], meeting_duration: timedelta
+    ) -> List[Tuple[datetime, datetime]]:
+    return []
+
+
+if __name__ == "__main__":
+    pass
